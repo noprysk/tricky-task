ab571484e45c4a2bfa875270e550a17d3d560f09
nazar.oprysk <nazar.oprysk@empirichealth.com>
2020-03-25 14:42:12 -0600

    Init commit

---
+++ src/main/java/nao/cycledev/trickytask/TrickyTaskApplication.java
@@ -0,0 +1,13 @@
+package nao.cycledev.trickytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrickyTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrickyTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nao/cycledev/trickytask/TrickyTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package nao.cycledev.trickytask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrickyTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
