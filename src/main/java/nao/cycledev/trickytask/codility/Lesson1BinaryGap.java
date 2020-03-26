a48af2586dfb86034322001e66f215e983f128f9
nazar.oprysk <nazar.oprysk@empirichealth.com>
2020-03-26 09:49:41 -0600

    Lesson 1 Binary Gap

--- src/main/java/nao/cycledev/trickytask/codility/Lesson1BinaryGap.java
+++ src/main/java/nao/cycledev/trickytask/codility/Lesson1BinaryGap.java
@@ -1,16 +1,16 @@
 package nao.cycledev.trickytask.codility;
 
 public class Lesson1BinaryGap {
-    public static int solution(int A) {
-        if (A < 1 || A > 2147483647) {
+    public static int solution(int N) {
+        if (N < 1 || N > 2147483647) {
             return 0;
         }
 
         int count = 0;
         int max = 0;
 
-        String value = Integer.toBinaryString(A);
-        System.out.println("Binary: " + value + "; for: " + A);
+        String value = Integer.toBinaryString(N);
+        System.out.println("Binary: " + value + "; for: " + N);
 
         for (Character c : value.toCharArray()) {
             if (c == '0') {
